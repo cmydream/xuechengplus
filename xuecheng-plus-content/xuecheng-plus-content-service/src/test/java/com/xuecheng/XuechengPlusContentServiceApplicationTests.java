e2e53b22ab0ab645d6accf6b97bf33117aa1e43d
cmy <cuimy_work@163.com>
2023-02-25 21:21:30 +0800

    课程添加分类树接口开发

--- xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/XuechengPlusContentServiceApplicationTests.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/XuechengPlusContentServiceApplicationTests.java
@@ -4,12 +4,16 @@ package com.xuecheng;
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.mapper.CourseBaseMapper;
+import com.xuecheng.content.model.dto.CourseCategoryTreeDto;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
 import com.xuecheng.content.service.CourseBaseInfoService;
+import com.xuecheng.content.service.CourseCategoryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class XuechengPlusContentServiceApplicationTests {
@@ -17,6 +21,9 @@ class XuechengPlusContentServiceApplicationTests {
     CourseBaseMapper courseBaseMapper;
     @Autowired
     CourseBaseInfoService courseBaseInfoService;
+
+    @Autowired
+    CourseCategoryService courseCategoryService;
 
     @Test
     void testCourseBaseMapper(){
@@ -30,6 +37,12 @@ class XuechengPlusContentServiceApplicationTests {
         PageResult<CourseBase> courseBasePageResult = courseBaseInfoService.queryCourseBaseList(pageParams, new QueryCourseParamsDto());
         System.out.println("-----------------------------"+courseBasePageResult);
 
+    }
+
+    @Test
+    void testcourseCategoryService(){
+        List<CourseCategoryTreeDto> courseCategoryTreeDtos = courseCategoryService.queryTreeNodes("1");
+        System.out.println(courseCategoryTreeDtos);
     }
 
 
--- xuecheng-plus-generator/src/main/java/com/xuecheng/generator/ContentCodeGenerator.java
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/generator/ContentCodeGenerator.java
@@ -26,11 +26,12 @@ public class ContentCodeGenerator {
 	private static final String[] TABLE_NAMES = new String[]{
 //			"mq_message",
 //			"mq_message_history"
-			 "course_base",
+/*			 "course_base",
 			 "course_market",
 			 "teachplan",
 			 "teachplan_media",
-			 "course_teacher",
+			 "course_teacher",*/
+			 "course_category",
 //			 "course_publish",
 //			 "course_publish_pre"
 //			"course_category"
